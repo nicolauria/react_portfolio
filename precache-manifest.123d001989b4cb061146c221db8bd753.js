a88bb1ed8ecdf90a9f423c1c45fffc8a5ed76bae
Nico Lauria <nflauria@outlook.com>
2020-05-21 15:03:54 -0700

    Updates

---
+++ precache-manifest.123d001989b4cb061146c221db8bd753.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4f08a069b0edfd61ad447844fcf119f",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "85916ce400daabd4712e",
+    "url": "/react_portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "3d88b2c65852b97d0911",
+    "url": "/react_portfolio/static/css/main.4d79f877.chunk.css"
+  },
+  {
+    "revision": "85916ce400daabd4712e",
+    "url": "/react_portfolio/static/js/2.5056b4fc.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/react_portfolio/static/js/2.5056b4fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d88b2c65852b97d0911",
+    "url": "/react_portfolio/static/js/main.99ac40ec.chunk.js"
+  },
+  {
+    "revision": "4901414a795d9e09b443",
+    "url": "/react_portfolio/static/js/runtime-main.08fd7df0.js"
+  }
+]);
